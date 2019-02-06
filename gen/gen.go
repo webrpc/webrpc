9e5bb1c1592642e8123ab7e82ca90b53cc8bcfda
Peter Kieltyka <peter.kieltyka@gmail.com>
2019-02-06 13:53:01 -0500

    remove cruft

--- gen/gen.go
+++ gen/gen.go
@@ -5,31 +5,6 @@ import (
 	"github.com/webrpc/webrpc/schema"
 )
 
-// the webrpc-gen tool goes here.
-// starting with support for Go server+client and TypeScript client
-
 func Gen(proto *schema.WebRPCSchema) (string, error) {
-
-	// vars := struct {
-	// 	Name string
-	// }{
-	// 	"joe",
-	// }
-
-	// t, err := template.New("go.client").Parse(GoClientTemplate)
-	// if err != nil {
-	// 	return "", err
-	// }
-
-	// buf := bytes.NewBuffer(nil)
-
-	// err = t.Execute(buf, vars)
-	// if err != nil {
-	// 	return "", err
-	// }
-
-	// return string(buf.Bytes()), nil
-
 	return golang.Gen(proto)
-
 }
--- gen/go_client.tpl.go
+++
@@ -1,5 +0,0 @@
-package gen
-
-var GoClientTemplate = `
-  hi {{ .Name }} :)
-`
