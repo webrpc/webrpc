35154d088ec49c620a0112c7f6fd0dbefb20214e
José Carlos Nieto <jose.carlos@menteslibres.net>
2019-02-06 12:04:31 +0000

    wip: add go target

---
+++ gen/targets/go/target.go
@@ -0,0 +1,108 @@
+package gen
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/webrpc/webrpc-go/schema"
+)
+
+func serviceMethodName(in schema.VarName) (string, error) {
+	s := string(in)
+	return strings.ToLower(s[0:1]) + s[1:], nil
+}
+
+func fieldTags(in []schema.MessageFieldTag) (string, error) {
+	if len(in) < 1 {
+		return "", nil
+	}
+	fieldTags := []string{}
+	for kk := range in {
+		for k := range in[kk] {
+			fieldTags = append(fieldTags, fmt.Sprintf(`%s:"%v"`, k, in[kk][k]))
+		}
+	}
+	return "`" + strings.Join(fieldTags, " ") + "`", nil
+}
+
+func newClientServiceName(in schema.VarName) (string, error) {
+	return "New" + string(in) + "Client", nil
+}
+
+func fieldType(in schema.VarType) (string, error) {
+	s := in.String()
+	return s, nil
+}
+
+func constPathPrefix(in schema.VarName) (string, error) {
+	return string(in) + "PathPrefix", nil
+}
+
+func countMethods(in []*schema.Method) (string, error) {
+	return strconv.Itoa(len(in)), nil
+}
+
+func clientServiceName(in schema.VarName) (string, error) {
+	s := string(in)
+	return strings.ToLower(s[0:1]) + s[1:] + "Client", nil
+}
+
+func methodInputName(in *schema.MethodArgument) string {
+	name := string(in.Name)
+	if name != "" {
+		return name
+	}
+	if in.Type != nil {
+		return in.Type.String()
+	}
+	return ""
+}
+
+func methodInputType(in *schema.MethodArgument) string {
+	// TODO: how do we know when we have a pointer?
+	return in.Type.String()
+}
+
+func methodInputs(in []*schema.MethodArgument) (string, error) {
+	inputs := []string{"ctx context.Context"}
+
+	for i := range in {
+		inputs = append(inputs, fmt.Sprintf("%s %s", methodInputName(in[i]), methodInputType(in[i])))
+	}
+
+	return strings.Join(inputs, ", "), nil
+}
+
+func methodOutputs(in []*schema.MethodArgument) (string, error) {
+	return "", nil
+}
+
+var templateFuncMap = map[string]interface{}{
+	"serviceMethodName":    serviceMethodName,
+	"fieldTags":            fieldTags,
+	"fieldType":            fieldType,
+	"newClientServiceName": newClientServiceName,
+	"constPathPrefix":      constPathPrefix,
+	"countMethods":         countMethods,
+	"clientServiceName":    clientServiceName,
+	"methodInputs":         methodInputs,
+	"methodOutputs":        methodOutputs,
+}
+
+func compile(s *schema.WebRPCSchema) ([]byte, error) {
+	tpl, err := template.New("").
+		Funcs(templateFuncMap).
+		Parse(goTemplate)
+
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(nil)
+	if err := tpl.Execute(buf, s); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
---
+++ gen/targets/go/target_test.go
@@ -0,0 +1,304 @@
+package gen
+
+import (
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webrpc/webrpc-go/schema"
+)
+
+var testMessages = []string{
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "messages": [
+    {
+      "name": "Kind",
+      "type": "enum",
+      "fields": [
+        {
+          "name": "USER",
+          "type": "uint32",
+          "value": "1"
+        },
+        {
+          "name": "ADMIN",
+          "type": "uint32",
+          "value": "2"
+        }
+      ]
+    }
+	]
+}`,
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "messages": [
+    {
+      "name": "Empty",
+      "type": "struct",
+      "fields": [
+      ]
+    }
+	]
+}`,
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "messages": [
+    {
+      "name": "GetUserRequest",
+      "type": "struct",
+      "fields": [
+        {
+          "name": "userID",
+          "type": "uint64",
+          "optional": false
+        }
+      ]
+    },
+    {
+      "name": "User",
+      "type": "struct",
+      "fields": [
+        {
+          "name": "ID",
+          "type": "uint64",
+          "optional": false,
+          "tags": [
+            { "json": "id" },
+            { "db": "id" }
+          ]
+        },
+        {
+          "name": "username",
+          "type": "string",
+          "optional": false,
+          "tags": [
+            { "json": "USERNAME" },
+            { "db": "username" }
+          ]
+        },
+        {
+          "name": "createdAt",
+          "type": "timestamp",
+          "optional": true,
+          "tags": [
+            { "json": "created_at,omitempty" },
+            { "db": "created_at" }
+          ]
+        }
+
+      ]
+    }
+	]
+}`,
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "services": [
+  ]
+}`,
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "services": [
+    {
+      "name": "ExampleService",
+      "methods": [
+        {
+          "name": "Ping",
+          "inputs": [],
+          "outputs": [
+            {
+              "name": "",
+              "type": "bool"
+            }
+          ]
+        },
+        {
+          "name": "GetUser",
+          "inputs": [
+            {
+              "name": "req",
+              "type": "GetUserRequest"
+            }
+          ],
+          "outputs": [
+            {
+              "name": "",
+              "type": "User"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}`,
+	`{
+  "schema": "webrpc/v0.0.1",
+  "app": "example/v0.0.1",
+  "messages": [
+    {
+      "name": "Kind",
+      "type": "enum",
+      "fields": [
+        {
+          "name": "USER",
+          "type": "uint32",
+          "value": "1"
+        },
+        {
+          "name": "ADMIN",
+          "type": "uint32",
+          "value": "2"
+        }
+      ]
+    },
+    {
+      "name": "Empty",
+      "type": "struct",
+      "fields": [
+      ]
+    },
+    {
+      "name": "GetUserRequest",
+      "type": "struct",
+      "fields": [
+        {
+          "name": "userID",
+          "type": "uint64",
+          "optional": false
+        }
+      ]
+    },
+    {
+      "name": "User",
+      "type": "struct",
+      "fields": [
+        {
+          "name": "ID",
+          "type": "uint64",
+          "optional": false,
+          "tags": [
+            { "json": "id" },
+            { "db": "id" }
+          ]
+        },
+        {
+          "name": "username",
+          "type": "string",
+          "optional": false,
+          "tags": [
+            { "json": "USERNAME" },
+            { "db": "username" }
+          ]
+        },
+        {
+          "name": "createdAt",
+          "type": "timestamp",
+          "optional": true,
+          "tags": [
+            { "json": "created_at,omitempty" },
+            { "db": "created_at" }
+          ]
+        }
+
+      ]
+    },
+    {
+      "name": "RandomStuff",
+      "type": "struct",
+      "fields": [
+        {
+          "name": "meta",
+          "type": "map<string,any>"
+        },
+        {
+          "name": "metaNestedExample",
+          "type": "map<string,map<string,uint32>>"
+        },
+        {
+          "name": "namesList",
+          "type": "[]string"
+        },
+        {
+          "name": "numsList",
+          "type": "[]int64"
+        },
+        {
+          "name": "doubleArray",
+          "type": "[][]string"
+        },
+        {
+          "name": "listOfMaps",
+          "type": "[]map<string,uint32>"
+        },
+        {
+          "name": "listOfUsers",
+          "type": "[]User"
+        },
+        {
+          "name": "mapOfUsers",
+          "type": "map<string,User>"
+        },
+        {
+          "name": "user",
+          "type": "User"
+        }
+      ]
+    }
+  ],
+  "services": [
+    {
+      "name": "ExampleService",
+      "methods": [
+        {
+          "name": "Ping",
+          "inputs": [],
+          "outputs": [
+            {
+              "name": "",
+              "type": "bool"
+            }
+          ]
+        },
+        {
+          "name": "GetUser",
+          "inputs": [
+            {
+              "name": "req",
+              "type": "GetUserRequest"
+            }
+          ],
+          "outputs": [
+            {
+              "name": "",
+              "type": "User"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}`,
+}
+
+func TestGenerateGo(t *testing.T) {
+	for _, input := range testMessages {
+		s, err := parseSchema([]byte(input))
+		assert.NoError(t, err)
+
+		buf, err := compile(s)
+		assert.NoError(t, err)
+		assert.NotNil(t, buf)
+
+		log.Printf("t: %s", string(buf))
+	}
+}
+
+func parseSchema(in []byte) (*schema.WebRPCSchema, error) {
+	s, err := schema.ParseSchema(in)
+	return s, err
+}
