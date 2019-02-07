46389044c1f688de9bbd0a830232f79d6138d376
Peter Kieltyka <peter.kieltyka@gmail.com>
2019-02-06 20:53:06 -0500

    webrpc-go, end-to-end tests

--- lib/webrpc-go/tests/rpc_test.go
+++ lib/webrpc-go/tests/rpc_test.go
@@ -1,57 +1,39 @@
-//go:generate ../../../bin/webrpc-gen -schema=sample.webrpc.json -target=go -pkg=proto -server -client -out=./proto/proto.gen.go
-package golang_test
+package tests
 
 import (
 	"context"
-	"net/http"
 	"testing"
 
-	"github.com/go-chi/chi"
-	"github.com/webrpc/webrpc/lib/webrpc-go"
+	"github.com/stretchr/testify/assert"
 	"github.com/webrpc/webrpc/lib/webrpc-go/tests/proto"
 )
 
-func NewWebService() http.Handler {
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("."))
-	})
-
-	r.Handle("/rpc/ExampleRPC/*", proto.NewExampleRPCServer(&exampleRPC{}))
-	r.Handle("/rpc/AnotherRPC/*", proto.NewAnotherRPCServer(&anotherRPC{}))
-
-	return r
+func TestPing(t *testing.T) {
+	resp, err := Client.Ping(context.Background())
+	assert.Equal(t, false, *resp)
+	assert.NoError(t, err)
 }
 
-type exampleRPC struct{}
-
-func (s *exampleRPC) Ping(ctx context.Context) (*bool, error) {
-	resp := false
-	return &resp, nil
-}
-
-func (s *exampleRPC) GetUser(ctx context.Context, req *proto.GetUserRequest) (*proto.User, error) {
-	if req.UserID == 911 {
-		return nil, webrpc.ErrorNotFound("unknown userID %d", 911)
-		// return nil, webrpc.Errorf(webrpc.ErrNotFound, "unknown userID %d", 911)
-		// return nil, webrpc.WrapError(webrpc.ErrNotFound, err, "unknown userID %d", 911)
+func TestGetUser(t *testing.T) {
+	{
+		resp, err := Client.GetUser(context.Background(), &proto.GetUserRequest{
+			UserID: 12,
+		})
+		assert.Equal(t, &proto.User{ID: 1, Username: "hihi"}, resp)
+		assert.NoError(t, err)
 	}
 
-	return &proto.User{
-		ID:       1,
-		Username: "hihi",
-	}, nil
-}
+	{
+		// Error case, expecting to receive an error
+		resp, err := Client.GetUser(context.Background(), &proto.GetUserRequest{
+			UserID: 911,
+		})
 
-type anotherRPC struct{}
+		// TODO: err should be webrpc.Error type
+		// so we can decode the .Code(), .Msg(), etc..
 
-func (s *anotherRPC) Owner(ctx context.Context) (*proto.User, error) {
-	return &proto.User{
-		ID:       42,
-		Username: "magic",
-	}, nil
-}
-
-func TestStuff(t *testing.T) {
-
+		assert.Nil(t, resp)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "not_found")
+	}
 }
