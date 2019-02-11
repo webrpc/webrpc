6aed315c6b69f2a48e1dcce446e4bfe93d73abd1
José Carlos Nieto <jose.carlos@menteslibres.net>
2019-02-11 23:07:23 +0000

    basic lexer

--- schema/ridl/ridl.go
+++ schema/ridl/ridl.go
@@ -1,12 +1,26 @@
 package ridl
 
+import (
+	"log"
+)
+
 type Tree struct {
 }
 
 type Token struct {
 }
 
-func Tokenize(input []byte) (*Tree, error) {
-	_ = Lex(string(input))
+func Tokenize(input string) (*Tree, error) {
+	lx := Lex(string(input))
+	for {
+		tok := <-lx.tokens
+		if tok.tt == tokenEOF {
+			break
+		}
+		if tok.tt == tokenNewLine || tok.tt == tokenSpace {
+			continue
+		}
+		log.Printf("tok: %#v", tok)
+	}
 	return nil, nil
 }
