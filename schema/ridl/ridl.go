778f7eec6611ab8248d617928a8937b13789bea7
José Carlos Nieto <jose.carlos@menteslibres.net>
2019-02-11 23:35:57 +0000

    add more token types

--- schema/ridl/ridl.go
+++ schema/ridl/ridl.go
@@ -14,13 +14,17 @@ func Tokenize(input string) (*Tree, error) {
 	lx := Lex(string(input))
 	for {
 		tok := <-lx.tokens
+		if tok.tt == tokenSpace {
+			continue
+		}
 		if tok.tt == tokenEOF {
 			break
 		}
-		if tok.tt == tokenNewLine || tok.tt == tokenSpace {
+		if tok.tt == tokenNewLine {
+			log.Printf("newline")
 			continue
 		}
-		log.Printf("tok: %#v", tok)
+		log.Printf("%v", tok)
 	}
 	return nil, nil
 }
