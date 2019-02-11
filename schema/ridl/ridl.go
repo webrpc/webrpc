287c021cefebadeab2b18b40a17f10206ddeb135
José Carlos Nieto <jose.carlos@menteslibres.net>
2019-02-11 20:46:38 +0000

    draft token types

--- schema/ridl/ridl.go
+++ schema/ridl/ridl.go
@@ -1,1 +1,30 @@
 package ridl
+
+type tokenType uint8
+
+const (
+	tokenSpace tokenType = iota
+	tokenNewLine
+	tokenDefine
+	tokenDefineValue
+	tokenImport
+	tokenImportValue
+	tokenEnum
+	tokenEnumType
+	tokenMessage
+	tokenService
+	tokenField
+	tokenMethod
+	tokenMethodType
+	tokenArgument
+	tokenArgumentType
+	tokenMeta
+	tokenComment
+)
+
+type Tree struct {
+}
+
+func Tokenize(input []byte) (*Tree, error) {
+	return nil, nil
+}
