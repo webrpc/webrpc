da35b2b5158e50517b812a9e2a80d6663c4c67fc
José Carlos Nieto <jose.carlos@menteslibres.net>
2019-02-12 14:23:56 +0000

    draft parser

--- schema/ridl/ridl_test.go
+++ schema/ridl/ridl_test.go
@@ -1,6 +1,7 @@
 package ridl
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
@@ -8,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestBasic(t *testing.T) {
+func SkipTestLexer(t *testing.T) {
 	buf := `
 
 
@@ -23,23 +24,76 @@ func TestBasic(t *testing.T) {
 
 foo=bar`
 
-	tokens, err := Tokenize(buf)
+	tokens, err := tokenize(buf)
 	assert.NoError(t, err)
 
 	log.Printf("buf: %v", string(buf))
 	log.Printf("tokens: %v", tokens)
 }
 
-func TestParse(t *testing.T) {
+func TestRidlHeader(t *testing.T) {
+	{
+		buf := `
+	ridl = v1
+	`
+		_, err := Parse(buf)
+		assert.NoError(t, err)
+	}
+	{
+		buf := `
+	ridl = v0
+	ridl = v1
+	`
+		_, err := Parse(buf)
+		assert.Error(t, err)
+	}
+}
+
+func TestHeaders(t *testing.T) {
+	{
+		buf := `
+	ridl = v1
+	version = v0.1.1
+	service = hello-webrpc
+	`
+		_, err := Parse(buf)
+		assert.NoError(t, err)
+	}
+}
+
+func TestHeadersImport(t *testing.T) {
+	{
+		input := `
+	ridl = v1
+		version = v0.1.1
+	service = hello-webrpc
+
+	import
+	- foo
+		- bar
+	`
+		schema, err := Parse(input)
+		assert.NoError(t, err)
+
+		log.Printf("schema: %v", schema)
+
+		buf, err := json.Marshal(schema)
+		assert.NoError(t, err)
+		log.Printf("schema JSON: %v", string(buf))
+
+	}
+}
+
+func SkipTestParse(t *testing.T) {
 	fp, err := os.Open("example1.ridl")
 	assert.NoError(t, err)
 
 	buf, err := ioutil.ReadAll(fp)
 	assert.NoError(t, err)
 
-	tokens, err := Tokenize(string(buf))
+	schema, err := Parse(string(buf))
 	assert.NoError(t, err)
 
 	log.Printf("buf: %v", string(buf))
-	log.Printf("tokens: %v", tokens)
+	log.Printf("schema: %v", schema)
 }
