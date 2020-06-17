a4ffbaafe81f81727272e9f9ea21a993f7074ff0
G.Becks <techstar.dev@hotmail.com>
2020-06-17 17:31:49 -0400

    add readme.md

--- _examples/hello-webrpc-dart/client/web/main.dart
+++ _examples/hello-webrpc-dart/client/web/main.dart
@@ -1,5 +1,40 @@
 import 'dart:html';
+import 'dart:math';
+
+import 'package:client/client.gen.dart';
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  final pingBtn = querySelector('#ping');
+  final getUserBtn = querySelector('#getUser');
+  final pingResultDiv = querySelector('#pingResult');
+  final getUserResultDiv = querySelector('#getUserResult');
+  final host = 'http://localhost:8080';
+  int userId() {
+    final min = 1;
+    final max = 5;
+    final rnd = Random();
+    return min + rnd.nextInt(max - min);
+  }
+
+  final client = ExampleServiceRpc(host: host);
+  pingBtn.onClick.listen((event) async {
+    await for (final result in client.ping()) {
+      result.maybeWhen(
+          ok: (data) => pingResultDiv.innerHtml = data.status.toString(),
+          orElse: () => pingResultDiv.innerHtml = 'server error occurred');
+    }
+  });
+
+  getUserBtn.onClick.listen((event) async {
+    final id = userId();
+    await for (final result in client.getUser(
+      userID: id,
+    )) {
+      result.maybeWhen(
+        ok: (data) =>
+            getUserResultDiv.innerHtml = '${data.user.username} received',
+        orElse: () => getUserResultDiv.innerHtml = 'no user found for id: $id',
+      );
+    }
+  });
 }
---
+++ _examples/hello-webrpc-dart/readme.md
@@ -0,0 +1,26 @@
+# webrpc hello-webrpc-dart
+
+* Server: Shelf (Dart)
+* Client: Web Browser (Dart)
+
+A simple example showcasing the use of webrpc dart for fullstack usage using [hello-api.ridl](hello-api.ridl). A more thorough example including flutter usage can be found [here](https://github.com/g5becks/Dart-WebRpc-Example).
+
+## Usage
+
+1. Install dart and run `pub global activate webdev`
+2. $ `make bootstrap` -- runs pub get on both ./server and ./client
+3. $ `make run-server`
+4. $ `make run-client`
+5. To regenerate the client and/or server run `make generate`
+
+Visit http://localhost:4444 !
+
+## Notes
+
+The cool thing about webrpc and other schema-driven rpc libraries (like grpc), is that you can
+generate a Go client for this node server just by running:
+
+`webrpc-gen -schema=service.ridl -target=go -pkg=proto -client -out=./proto/client.gen.go`
+
+and tada, your Go programs now have full type definitions and network communication to the node
+server!
