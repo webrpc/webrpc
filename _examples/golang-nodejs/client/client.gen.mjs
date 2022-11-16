7bcb3c44cf389259aaca5ff7ac9cf19c7c8aeb3f
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-11-16 16:48:09 +0100

    Apply the fetch wrapper fixes from typescript/javascript generators (#136)
    https://github.com/webrpc/gen-typescript/pull/1
    https://github.com/webrpc/gen-javascript/pull/1

--- _examples/golang-nodejs/client/client.gen.mjs
+++ _examples/golang-nodejs/client/client.gen.mjs
@@ -175,7 +175,7 @@ export class ExampleService {
   constructor(hostname, fetch) {
     this.path = '/rpc/ExampleService/'
     this.hostname = hostname
-    this.fetch = fetch
+    this.fetch = () => fetch
   }
 
   url(name) {
--- _examples/hello-webrpc/webapp/client.gen.js
+++ _examples/hello-webrpc/webapp/client.gen.js
@@ -79,7 +79,7 @@ class ExampleService {
   constructor(hostname, fetch) {
     this.path = '/rpc/ExampleService/'
     this.hostname = hostname
-    this.fetch = fetch
+    this.fetch = () => fetch
   }
 
   url(name) {
