c3ea1deb043776d76e0f0eae55f11558495be6b8
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-12-21 20:58:51 +0100

    Move VERSION to top-level package

---
+++ version.go
@@ -0,0 +1,12 @@
+package webrpc
+
+// VERSION defines version of webrpc tools, such as webrpc-gen.
+//
+// Please update major/minor version by hand when changing webrpc-gen
+// Template Functions API or when making any backward-incompatible changes.
+// The patch value is automatically updated with the latest git tag in CI.
+//
+// Available as {{.WebrpcGenVersion}} in generator templates. Templates can
+// check against {{.WebrpcGenVersion}} to require minimal major/minor version
+// to guarantees certain Template functions API.
+var VERSION = "v0.8.x-dev"
