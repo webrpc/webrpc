3f2508a48d7fef62c76e2cde79d8c1f8227595ea
Vojtech Vitek <vojtech.vitek@golang.cz>
2022-12-30 18:28:24 +0100

    Improve interoperability test suite: Add more RPC methods to TestApi (#181)
    * Improve interoperability test server
    
    * Rename tests/schema/api.ridl to test.ridl
    
    * Rename ComplexApi to TestApi
    
    * Implement new TestApi RPC methods
    
    * Define fixtures, use cpm.Equal() and cpm.Diff() for comparison
    
    * Reverse cmp.Diff() arguments to make diff logical
    
    * Fix interoperability tests by using float64 in fixtures
    
    * Fix webrpc-test build

---
+++ tests/interoperability_test.go
@@ -0,0 +1,22 @@
+package tests
+
+//go:generate webrpc-gen -schema=./schema/test.ridl -target=golang -pkg=client -client -out=./client/client.gen.go
+//go:generate webrpc-gen -schema=./schema/test.ridl -target=golang -pkg=server -server -out=./server/server.gen.go
+
+import (
+	"context"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webrpc/webrpc/tests/client"
+	"github.com/webrpc/webrpc/tests/server"
+)
+
+func TestInteroperability(t *testing.T) {
+	srv := httptest.NewServer(server.NewTestApiServer(&server.TestServer{}))
+	defer srv.Close()
+
+	err := client.RunTests(context.Background(), srv.URL)
+	assert.NoError(t, err)
+}
--- tests/server/server.go
+++ tests/server/server.go
@@ -1,190 +1,187 @@
 package server
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
 
-type ComplexServer struct{}
+	"github.com/google/go-cmp/cmp"
+)
 
-func (*ComplexServer) GetMeta() map[string]interface{} {
-	return map[string]interface{}{
-		"1": "23",
-		"2": 24,
-	}
+type TestServer struct{}
+
+func (c *TestServer) GetEmpty(ctx context.Context) error {
+	return nil
 }
 
-func (*ComplexServer) GetMetaNestedExample() map[string]map[string]uint32 {
-	return map[string]map[string]uint32{
+func (c *TestServer) GetError(ctx context.Context) error {
+	return ErrorInternal("internal error")
+}
+
+func (c *TestServer) GetOne(ctx context.Context) (*Simple, error) {
+	return &fixtureOne, nil
+}
+
+func (c *TestServer) SendOne(ctx context.Context, one *Simple) error {
+	if !cmp.Equal(&fixtureOne, one) {
+		return Errorf(ErrInvalidArgument, "%q:\n%s", "one", cmp.Diff(&fixtureOne, one))
+	}
+
+	return nil
+}
+
+func (c *TestServer) GetMulti(ctx context.Context) (*Simple, *Simple, *Simple, error) {
+	return &fixtureOne, &fixtureTwo, &fixtureThree, nil
+}
+
+func (c *TestServer) SendMulti(ctx context.Context, one, two, three *Simple) error {
+	if !cmp.Equal(&fixtureOne, one) {
+		return Errorf(ErrInvalidArgument, "%q:\n%s", "one", cmp.Diff(&fixtureOne, one))
+	}
+	if !cmp.Equal(&fixtureTwo, two) {
+		return Errorf(ErrInvalidArgument, "%q:\n%s", "two", cmp.Diff(&fixtureTwo, two))
+	}
+	if !cmp.Equal(&fixtureThree, three) {
+		return Errorf(ErrInvalidArgument, "%q:\n%s", "three", cmp.Diff(&fixtureThree, three))
+	}
+
+	return nil
+}
+
+func (c *TestServer) GetComplex(ctx context.Context) (*Complex, error) {
+	return &fixtureComplex, nil
+}
+
+func (c *TestServer) SendComplex(ctx context.Context, complex *Complex) error {
+	if !cmp.Equal(&fixtureComplex, complex) {
+		return Errorf(ErrInvalidArgument, "%q:\n%s", "complex", cmp.Diff(&fixtureComplex, complex))
+	}
+
+	return nil
+}
+
+// Fixtures
+var (
+	fixtureOne = Simple{
+		Id:   1,
+		Name: "one",
+	}
+	fixtureTwo = Simple{
+		Id:   2,
+		Name: "two",
+	}
+	fixtureThree = Simple{
+		Id:   3,
+		Name: "three",
+	}
+
+	meta = map[string]interface{}{
+		"1": "23",
+		"2": float64(24), // Go JSON unmarshaler uses float64 for numbers by default.
+	}
+	metaNested = map[string]map[string]uint32{
 		"1": {
 			"2": 1,
 		},
 	}
-}
-
-func (*ComplexServer) GetNamesList() []string {
-	return []string{"John", "Alice", "Jakob"}
-}
-
-func (*ComplexServer) GetNumsList() []int64 {
-	return []int64{1, 2, 3, 4534643543}
-}
-
-func (*ComplexServer) GetDoubleArray() [][]string {
-	return [][]string{{"testing"}, {"api"}}
-}
-
-func (*ComplexServer) ListOfMaps() []map[string]uint32 {
-	return []map[string]uint32{
+	namesList   = []string{"John", "Alice", "Jakob"}
+	numsList    = []int64{1, 2, 3, 4534643543}
+	doubleArray = [][]string{{"testing"}, {"api"}}
+	listOfMaps  = []map[string]uint32{
 		{
 			"john":  1,
 			"alice": 2,
 			"Jakob": 251,
 		},
 	}
-}
-
-func (*ComplexServer) ListOfUsers() []*User {
-	return []*User{
+	listOfUsers = []*User{
 		{
 			ID:       1,
 			Username: "John-Doe",
 			Role:     "admin",
 		},
 	}
-}
-
-func (*ComplexServer) MapOfUsers() map[string]*User {
-	return map[string]*User{
+	mapOfUsers = map[string]*User{
 		"admin": {
 			ID:       1,
 			Username: "John-Doe",
 			Role:     "admin",
 		},
 	}
-}
-
-func (*ComplexServer) GetUser() *User {
-	return &User{
+	user = &User{
 		ID:       1,
 		Username: "John-Doe",
 		Role:     "admin",
 	}
+	available = Status_AVAILABLE
+
+	fixtureComplex = Complex{
+		Meta:              meta,
+		MetaNestedExample: metaNested,
+		NamesList:         namesList,
+		NumsList:          numsList,
+		DoubleArray:       doubleArray,
+		ListOfMaps:        listOfMaps,
+		ListOfUsers:       listOfUsers,
+		MapOfUsers:        mapOfUsers,
+		User:              user,
+		Enum:              &available,
+	}
+)
+
+func RunTestServer(addr string, timeout time.Duration) (*testServer, error) {
+	srv := &testServer{
+		Server: &http.Server{
+			Addr:    addr,
+			Handler: NewTestApiServer(&TestServer{}),
+		},
+		closed: make(chan struct{}),
+	}
+
+	if timeout > 0 {
+		go func() {
+			timeoutCtx, cancel := context.WithTimeout(context.Background(), timeout)
+			defer cancel()
+
+			select {
+			case <-srv.closed:
+
+			case <-timeoutCtx.Done():
+				gracefulShutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+				defer cancel()
+
+				srv.err = srv.Shutdown(gracefulShutdownCtx)
+				close(srv.closed)
+			}
+		}()
+	}
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to bind %v: %w", addr, err)
+	}
+
+	go srv.Serve(l)
+
+	return srv, nil
 }
 
-func (c *ComplexServer) SendComplex(ctx context.Context, complexType *ComplexType) error {
-	if _, ok := complexType.Meta["1"]; !ok {
-		return Errorf(ErrInvalidArgument, "meta: key %q does not exist", 1)
-	}
-	if complexType.Meta["1"] != "23" {
-		return Errorf(ErrInvalidArgument, "meta: value in key '1' is not equal to '23'")
-	}
-
-	if _, ok := complexType.Meta["2"]; !ok {
-		return Errorf(ErrInvalidArgument, "meta: key %q does not exist", 2)
-	}
-	if complexType.Meta["2"] != float64(24) {
-		return Errorf(ErrInvalidArgument, "meta: value in key '2' is not equal to '24'")
-	}
-
-	metaNestedExample := c.GetMetaNestedExample()
-	if _, ok := metaNestedExample["1"]; !ok {
-		return Errorf(ErrInvalidArgument, "meta nested: key [%q] does not exist in meta nested structure", 1)
-	}
-	if _, ok := metaNestedExample["1"]["2"]; !ok {
-		return Errorf(ErrInvalidArgument, "meta nested: key [1][%q] does not exist", 2)
-	}
-	if metaNestedExample["1"]["2"] != 1 {
-		return Errorf(ErrInvalidArgument, "meta nested: expected value 1, actual: %v", metaNestedExample["1"]["2"])
-	}
-
-	namesList := c.GetNamesList()
-	for index, val := range complexType.NamesList {
-		if val != namesList[index] {
-			return Errorf(ErrInvalidArgument, "names list: expected value %q, given %q", val, namesList[index])
-		}
-	}
-
-	numsList := c.GetNumsList()
-	for index, val := range complexType.NumsList {
-		if val != numsList[index] {
-			return Errorf(ErrInvalidArgument, "nums list: expected value %q, given %q", val, numsList[index])
-		}
-	}
-
-	doubleArray := c.GetDoubleArray()
-	for i, arr := range complexType.DoubleArray {
-		if arr[0] != doubleArray[i][0] {
-			return Errorf(ErrInvalidArgument, "double array: expected value %q, given %q", arr[0], doubleArray[i][0])
-		}
-	}
-
-	listOfMaps := c.ListOfMaps()
-	for i, m := range complexType.ListOfMaps {
-		if m["john"] != listOfMaps[i]["john"] {
-			return Errorf(ErrInvalidArgument, "list of maps: expected value %q, given %q", m["john"], listOfMaps[i]["john"])
-		}
-		if m["alice"] != listOfMaps[i]["alice"] {
-			return Errorf(ErrInvalidArgument, "list of maps: expected value %q, given %q", m["alice"], listOfMaps[i]["alice"])
-		}
-		if m["Jakob"] != listOfMaps[i]["Jakob"] {
-			return Errorf(ErrInvalidArgument, "list of maps: expected value %q, given %q", m["Jakob"], listOfMaps[i]["Jakob"])
-		}
-	}
-
-	listOfUsers := c.ListOfUsers()
-	if complexType.ListOfUsers[0].ID != listOfUsers[0].ID {
-		return Errorf(ErrInvalidArgument, "list of users: expected value %q, given %q", listOfUsers[0].ID, complexType.ListOfUsers[0].ID)
-	}
-	if complexType.ListOfUsers[0].Role != listOfUsers[0].Role {
-		return Errorf(ErrInvalidArgument, "list of users: expected value %q, given %q", listOfUsers[0].Role, complexType.ListOfUsers[0].Role)
-	}
-	if complexType.ListOfUsers[0].Username != listOfUsers[0].Username {
-		return Errorf(ErrInvalidArgument, "list of users: expected value %q, given %q", listOfUsers[0].Username, complexType.ListOfUsers[0].Username)
-	}
-
-	mapOfUsers := c.MapOfUsers()
-	if _, ok := complexType.MapOfUsers["admin"]; !ok {
-		return Errorf(ErrInvalidArgument, "map of users: expected 'admin' key to exist in map")
-	}
-	if complexType.MapOfUsers["admin"].ID != mapOfUsers["admin"].ID {
-		return Errorf(ErrInvalidArgument, "map of users: expected value %q, given %q", mapOfUsers["admin"].ID, complexType.MapOfUsers["admin"].ID)
-	}
-	if complexType.MapOfUsers["admin"].Role != mapOfUsers["admin"].Role {
-		return Errorf(ErrInvalidArgument, "map of users: expected value %q, given %q", mapOfUsers["admin"].Role, complexType.MapOfUsers["admin"].Role)
-	}
-	if complexType.MapOfUsers["admin"].Username != mapOfUsers["admin"].Username {
-		return Errorf(ErrInvalidArgument, "map of users: expected value %q, given %q", mapOfUsers["admin"].Username, complexType.MapOfUsers["admin"].Username)
-	}
-
-	user := c.GetUser()
-	if complexType.User.ID != user.ID {
-		return Errorf(ErrInvalidArgument, "user: expected value %q, given %q", complexType.User.ID, user.ID)
-	}
-	if complexType.User.Role != user.Role {
-		return Errorf(ErrInvalidArgument, "user: expected value %q, given %q", complexType.User.Role, user.Role)
-	}
-	if complexType.User.Username != user.Username {
-		return Errorf(ErrInvalidArgument, "user: expected value %q, given %q", complexType.User.Username, user.Username)
-	}
-
-	if complexType.Enum.String() != Status_AVAILABLE.String() {
-		return Errorf(ErrInvalidArgument, "enum: expected value %q, given %q", Status_AVAILABLE, complexType.Enum.String())
-	}
-
-	return nil
+type testServer struct {
+	*http.Server
+	closed chan struct{}
+	err    error
 }
 
-func (c *ComplexServer) GetComplex(ctx context.Context) (*ComplexType, error) {
-	status := Status_AVAILABLE
+func (srv *testServer) Wait() error {
+	<-srv.closed
+	return srv.err
+}
 
-	return &ComplexType{
-		Meta:              c.GetMeta(),
-		MetaNestedExample: c.GetMetaNestedExample(),
-		NamesList:         c.GetNamesList(),
-		NumsList:          c.GetNumsList(),
-		DoubleArray:       c.GetDoubleArray(),
-		ListOfMaps:        c.ListOfMaps(),
-		ListOfUsers:       c.ListOfUsers(),
-		MapOfUsers:        c.MapOfUsers(),
-		User:              c.GetUser(),
-		Enum:              &status,
-	}, nil
+func (srv *testServer) Close() error {
+	gracefulShutdownCtx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	return srv.Shutdown(gracefulShutdownCtx)
 }
