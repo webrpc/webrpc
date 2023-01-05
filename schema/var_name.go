ea5061f123eda93a6f5fa789ce8915854ba58315
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-01-05 15:38:14 +0100

    Remove unused schema.VarName methods

--- schema/var_name.go
+++ schema/var_name.go
@@ -1,27 +1,3 @@
 package schema
 
-import (
-	"strings"
-)
-
 type VarName string
-
-// TitleDowncase will downcase the first letter of a string,
-// ie. convert a name form 'FirstName' to 'firstName'
-func (v VarName) TitleDowncase() string {
-	if v == "" {
-		return ""
-	}
-	s := string(v)
-	return strings.ToLower(s[0:1]) + s[1:]
-}
-
-// TitleUpcase will upcase the first letter of a string,
-// ie. convert a name form 'firstName' to 'FirstName'
-func (v VarName) TitleUpcase() string {
-	if v == "" {
-		return ""
-	}
-	s := string(v)
-	return strings.ToUpper(s[0:1]) + s[1:]
-}
