a37356df419b960187eb027ae5ee2047f7966eef
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-01-13 23:28:00 +0100

    Add golden JSON schema file test to tests/

---
+++ tests/schema_test.go
@@ -0,0 +1,39 @@
+package tests
+
+import (
+	"flag"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+	"github.com/webrpc/webrpc/schema/ridl"
+)
+
+var updateFlag = flag.String("update", "", "update golden file to match tests' current behavior")
+
+func TestRIDLSchemaAgainstJSON(t *testing.T) {
+	exampleDirFS := os.DirFS("./schema")
+
+	schema, err := ridl.NewParser(exampleDirFS, "test.ridl").Parse()
+	assert.NoError(t, err)
+
+	jsonSchema, err := schema.ToJSON(true)
+	assert.NoError(t, err)
+
+	current := []byte(jsonSchema)
+
+	golden, err := ioutil.ReadFile("./_testdata/test.golden.json")
+	assert.NoError(t, err)
+
+	if *updateFlag == "./_testdata/test.golden.json" {
+		assert.NoError(t, os.WriteFile("./_testdata/test.golden.json", current, 0644))
+		return
+	}
+
+	if !cmp.Equal(golden, current) {
+		t.Error(cmp.Diff(golden, current))
+		t.Log("To update the golden file, run go test -update=./_testdata/test.golden.json")
+	}
+}
