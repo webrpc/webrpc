08b402c32006c278022580b073826396650e9066
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-06-05 17:37:16 +0200

    Add `webrpc-test -printJSON'

--- tests/embed.go
+++ tests/embed.go
@@ -1,12 +1,22 @@
 package tests
 
 import (
+	"embed"
 	_ "embed"
+
+	"github.com/webrpc/webrpc/schema/ridl"
 )
 
 //go:embed schema/test.ridl
-var testSchema string
+var fs embed.FS
 
-func GetSchema() string {
-	return testSchema
+func GetRIDLSchema() string {
+	data, _ := fs.ReadFile("schema/test.ridl")
+	return string(data)
+}
+
+func GetJSONSchema() string {
+	schema, _ := ridl.NewParser(fs, "schema/test.ridl").Parse()
+	data, _ := schema.ToJSON()
+	return data
 }
