54390e67e52cba28331145798855cef42ccfc7ec
Peter Kieltyka <peter.kieltyka@gmail.com>
2023-07-23 20:33:42 -0400

    errors, add WebrpcInternalError and set HTTPStatus 400 default on ErrWebrpcRequestFailed

--- gen/errors.go
+++ gen/errors.go
@@ -4,12 +4,13 @@ import "github.com/webrpc/webrpc/schema"
 
 var WebrpcErrors = []*schema.Error{
 	{Code: 0, Name: "WebrpcEndpoint", Message: "endpoint error", HTTPStatus: 400},
-	{Code: -1, Name: "WebrpcRequestFailed", Message: "request failed"},
+	{Code: -1, Name: "WebrpcRequestFailed", Message: "request failed", HTTPStatus: 400},
 	{Code: -2, Name: "WebrpcBadRoute", Message: "bad route", HTTPStatus: 404},
 	{Code: -3, Name: "WebrpcBadMethod", Message: "bad method", HTTPStatus: 405},
 	{Code: -4, Name: "WebrpcBadRequest", Message: "bad request", HTTPStatus: 400},
 	{Code: -5, Name: "WebrpcBadResponse", Message: "bad response", HTTPStatus: 500},
 	{Code: -6, Name: "WebrpcServerPanic", Message: "server panic", HTTPStatus: 500},
+	{Code: -7, Name: "WebrpcInternalError", Message: "internal error", HTTPStatus: 500},
 	// Note: Do not change existing values. Append only.
 	// Keep the list short. Code and Name must be unique.
 }
