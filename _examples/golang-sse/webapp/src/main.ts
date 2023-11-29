6f4b0aa1bfa80b159cbbf015b837c87dbb0f4985
Alexander Kolberg <alexander.kolberg@alida.com>
2023-11-29 10:49:53 +0100

    add comments

--- _examples/golang-sse/webapp/src/main.ts
+++ _examples/golang-sse/webapp/src/main.ts
@@ -1,13 +1,14 @@
-/// <reference lib="dom" />
-
 import { effect, signal } from "@preact/signals-core";
 import { Chat, Message } from "./rpc";
 import "./style.css";
 
+//Create client
 const api = new Chat("http://localhost:5173", fetch);
 
+//Create signal for messages
 const messages = signal<Message[]>([]);
 
+//Create message handlers
 const onMessage = (message: Message) => {
   messages.value = [...messages.value, message];
 };
@@ -24,11 +25,13 @@ const onClose = () => {
   console.log("Conection closed");
 };
 
+//Subscribe to messages
 api.subscribeMessages(
   { serverTimeoutSec: 10 },
   { onMessage, onError, onOpen, onClose }
 );
 
+// Update chatbox
 const chatbox = document.querySelector("#chatbox");
 
 effect(() => {
@@ -45,22 +48,9 @@ effect(() => {
   }
 });
 
+// Send new message on submit
 const form = document.querySelector("form") as HTMLFormElement;
-
-const names = [
-  "Chuck Norris",
-  "Mr. Bean",
-  "Bugs Bunny",
-  "Homer Simpson",
-  "SpongeBob",
-  "Patrick Star",
-  "Pikachu",
-  "Mario",
-  "Luigi",
-  "Yoda",
-];
-const randomIndex = Math.floor(Math.random() * names.length);
-const authorName = names[randomIndex];
+const authorName = randomUserName();
 
 function sendMessage(event: Event) {
   const textField = form.elements.namedItem("text") as HTMLInputElement;
@@ -77,3 +67,20 @@ function sendMessage(event: Event) {
 }
 
 form.addEventListener("submit", sendMessage);
+
+function randomUserName() {
+  const names = [
+    "Chuck Norris",
+    "Mr. Bean",
+    "Bugs Bunny",
+    "Homer Simpson",
+    "SpongeBob",
+    "Patrick Star",
+    "Pikachu",
+    "Mario",
+    "Luigi",
+    "Yoda",
+  ];
+  const randomIndex = Math.floor(Math.random() * names.length);
+  return names[randomIndex];
+}
