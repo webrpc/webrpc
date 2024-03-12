550333d6e29d0b0aeb0f50696939b00293009a82
Vojtech Vitek <vojtech.vitek@golang.cz>
2024-03-13 00:17:23 +0100

    Infer webrpc version if imported from another Go module
    Useful if you import webrpc into your Go project and invoke it via
    go run github.com/webrpc/webrpc/cmd/webrpc-gen.

--- version.go
+++ version.go
@@ -1,10 +1,38 @@
 package webrpc
 
-// VERSION defines version of webrpc tools, such as webrpc-gen.
+import (
+	"os/exec"
+	"strings"
+)
+
+// VERSION of webrpc tooling and webrpc-gen Template Functions API.
+// Available as {{.WebrpcGenVersion}} variable in Go templates.
 //
-// Please update major/minor version when making any incompatible changes
-// to the webrpc schema or webrpc-gen Template Functions API.
-// The patch value is automatically updated with the latest git tag in CI.
-//
-// Version is available as {{.WebrpcGenVersion}} in generator templates.
-var VERSION = "v0.15.2-dev"
+// The actual value is injected into the binary in the release CI step.
+var VERSION = ""
+
+func init() {
+	if VERSION == "" {
+		VERSION = getVersion()
+	}
+}
+
+// getVersion tries to get version of the github.com/webrpc/webrpc module from
+// current go.mod file. This is useful when running webrpc-gen from within
+// another Go module using `go run github.com/webrpc/webrpc/cmd/webrpc-gen'.
+func getVersion() string {
+	out, err := exec.Command("go", "list", "-m", "github.com/webrpc/webrpc").Output()
+	if err != nil {
+		return "development"
+	}
+
+	// $ go list -m github.com/webrpc/webrpc
+	// github.com/webrpc/webrpc v0.13.1\n
+	parts := strings.Split(string(out), " ")
+	if len(parts) < 2 {
+		return "development"
+	}
+
+	// v0.13.1
+	return parts[1]
+}
