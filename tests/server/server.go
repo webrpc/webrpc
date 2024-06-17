e7ac164bbcb75240fc6d71b8a39f32fba9bb7fa6
Alex Guerrieri <ag@horizon.io>
2024-06-17 13:34:15 +0200

    update tests

--- tests/server/server.go
+++ tests/server/server.go
@@ -63,6 +63,14 @@ func (c *TestServer) SendComplex(ctx context.Context, complex *Complex) error {
 	return nil
 }
 
+func (c *TestServer) GetEnumList(ctx context.Context) ([]Status, error) {
+	return fixtureComplex.EnumList, nil
+}
+
+func (c *TestServer) GetEnumMap(ctx context.Context) (map[Access]uint64, error) {
+	return fixtureComplex.EnumMap, nil
+}
+
 func (c *TestServer) GetSchemaError(ctx context.Context, code int) error {
 	switch code {
 	case 0:
@@ -173,6 +181,8 @@ var (
 		MapOfUsers:        mapOfUsers,
 		User:              user,
 		Status:            Status_AVAILABLE,
+		EnumMap:           map[Access]uint64{Access_READ: 1, Access_WRITE: 2},
+		EnumList:          []Status{Status_AVAILABLE, Status_NOT_AVAILABLE},
 	}
 )
 
