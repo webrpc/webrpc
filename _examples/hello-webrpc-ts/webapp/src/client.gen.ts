55af2d6cacac0e3f792859f10db31114a92f07a2
Lukas Jenicek <lukas.jenicek5@gmail.com>
2024-11-04 11:40:55 +0100

    update gen-typescript to v0.15.1

--- _examples/hello-webrpc-ts/webapp/src/client.gen.ts
+++ _examples/hello-webrpc-ts/webapp/src/client.gen.ts
@@ -7,7 +7,7 @@
 
 export const WebrpcHeader = "Webrpc"
 
-export const WebrpcHeaderValue = "webrpc;gen-typescript@v0.15.0;hello-webrpc@v1.0.0"
+export const WebrpcHeaderValue = "webrpc;gen-typescript@v0.15.1;hello-webrpc@v1.0.0"
 
 // WebRPC description and code-gen version
 export const WebRPCVersion = "v1"
@@ -182,7 +182,7 @@ export class ExampleService implements ExampleService {
 }
 
   const createHTTPRequest = (body: object = {}, headers: object = {}, signal: AbortSignal | null = null): object => {
-  const reqHeaders = { ...headers, 'Content-Type': 'application/json' }
+  const reqHeaders: {[key: string]: string} = { ...headers, 'Content-Type': 'application/json' }
   reqHeaders[WebrpcHeader] = WebrpcHeaderValue
 
   return {
