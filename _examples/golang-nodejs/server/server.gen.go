4395ef9b6a00a81a7912b8a26554fb0af9db70b4
VojtechVitek <139342+VojtechVitek@users.noreply.github.com>
2025-03-13 21:34:02 +0000

    [AUTOMATED] make generate

--- _examples/golang-nodejs/server/server.gen.go
+++ _examples/golang-nodejs/server/server.gen.go
@@ -18,7 +18,7 @@ import (
 
 const WebrpcHeader = "Webrpc"
 
-const WebrpcHeaderValue = "webrpc;gen-golang@v0.18.1;example@ v0.0.1"
+const WebrpcHeaderValue = "webrpc;gen-golang@v0.18.2;example@ v0.0.1"
 
 // WebRPC description and code-gen version
 func WebRPCVersion() string {
--- _examples/hello-webrpc-ts/server/hello_api.gen.go
+++ _examples/hello-webrpc-ts/server/hello_api.gen.go
@@ -18,7 +18,7 @@ import (
 
 const WebrpcHeader = "Webrpc"
 
-const WebrpcHeaderValue = "webrpc;gen-golang@v0.18.1;hello-webrpc@v1.0.0"
+const WebrpcHeaderValue = "webrpc;gen-golang@v0.18.2;hello-webrpc@v1.0.0"
 
 // WebRPC description and code-gen version
 func WebRPCVersion() string {
