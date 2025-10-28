27103bf0a0a9b7332d2e5bbff072fdd53f4278d8
Peter Kieltyka <peter.kieltyka@gmail.com>
2025-10-28 15:21:49 -0400

    update examples gen outputs

--- _examples/hello-webrpc/webapp/client.gen.ts
+++ _examples/hello-webrpc/webapp/client.gen.ts
@@ -420,7 +420,7 @@ export const webrpcErrorByCode: { [code: number]: any } = {
 
 export const WebrpcHeader = "Webrpc"
 
-export const WebrpcHeaderValue = "webrpc;gen-typescript@v0.22.0;hello-webrpc@v1.0.0"
+export const WebrpcHeaderValue = "webrpc;gen-typescript@v0.22.2;hello-webrpc@v1.0.0"
 
 type WebrpcGenVersions = {
   WebrpcGenVersion: string;
