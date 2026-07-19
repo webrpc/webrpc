1fb6a7e07b8da98d8c89d87e6f700dd824420622
VojtechVitek <139342+VojtechVitek@users.noreply.github.com>
2026-07-19 15:50:57 +0000

    chore(deps): update webrpc/gen-golang@v0.32.2

--- _examples/golang-basics/internal/internal.gen.go
+++ _examples/golang-basics/internal/internal.gen.go
@@ -717,47 +717,6 @@ func RespondWithError(w http.ResponseWriter, err error) {
 	w.Write(respBody)
 }
 
-type sendErrorFunc func(w http.ResponseWriter, r *http.Request, rpcErr WebRPCError)
-
-func succinctHandler[I any, O any](method string, fn func(context.Context, I) (O, error), sendError sendErrorFunc) func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		ctx = context.WithValue(ctx, MethodNameCtxKey, method)
-
-		reqBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			sendError(w, r, ErrWebrpcBadRequest.WithCausef("failed to read request data: %w", err))
-			return
-		}
-		defer r.Body.Close()
-
-		var reqPayload I
-		if err := json.Unmarshal(reqBody, &reqPayload); err != nil {
-			sendError(w, r, ErrWebrpcBadRequest.WithCausef("failed to unmarshal request data: %w", err))
-			return
-		}
-
-		respPayload, err := fn(ctx, reqPayload)
-		if err != nil {
-			rpcErr, ok := err.(WebRPCError)
-			if !ok {
-				rpcErr = ErrWebrpcEndpoint.WithCause(err)
-			}
-			sendError(w, r, rpcErr)
-			return
-		}
-
-		respBody, err := json.Marshal(respPayload)
-		if err != nil {
-			sendError(w, r, ErrWebrpcBadResponse.WithCausef("failed to marshal json response: %w", err))
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(respBody)
-	}
-}
-
 type method struct {
 	name        string
 	service     string
